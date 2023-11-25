1b755e2cedfae5ee19adf4aafdc63f17f68d955e
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2023-11-25 18:39:09 +0530

    Images loaded

--- flappy bird.py
+++ flappy bird.py
@@ -3,3 +3,15 @@ import random
 import os
 import time
 import neat
+
+# Window size
+WIN_HEIGHT = 800
+WIN_WIDTH = 600
+
+# Load images
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+
