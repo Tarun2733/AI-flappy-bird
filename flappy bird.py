4d593534f9c55ef7be81ac1381ea36a9c2ad957b
Tarun2733 <taruntiwari.mighty4113@gmail.com>
2024-01-01 19:28:23 +0530

    Making the logic for the bird movement

--- flappy bird.py
+++ flappy bird.py
@@ -21,8 +21,8 @@ class Bird:
     ANIMATION_TIME = 5               # How long each bird animation will last
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = x                  # Starting position
+        self.y = y                  # Starting position
         self.tilt = 0               # How much the image is tilted
         self.tick_count = 0         # Physics of the bird
         self.vel = 0                # Velocity
@@ -36,6 +36,16 @@ class Bird:
         self.height = self.y        # Where the bird jumped from
     
     def move(self):
+        self.tick_count += 1                                  # How many times we moved since the last jump
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2 # How many pixels we move up or down
+        if d >= 16:                                           
+            d = 16
+        if d < 0:
+            d -= 2
+        self.y = self.y + d                                    # Move the bird up or down
+
+        if d < 0 or self.y < self.height + 50:                 # If we are moving up or we are above the jump height
+            
 
 
 while True:
